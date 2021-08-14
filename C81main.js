c0ed6a162ca377dccb1f4e0fc1ad1ed43f7c2e58
patelpranay0610 <82006290+patelpranay0610@users.noreply.github.com>
2021-08-13 20:24:28 -0400

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,11 +1,11 @@
 canvas=document.getElementById("myCanvas");
-ctx=canvas.getcontext("2d");
+ctx=canvas.getContext("2d");
 color="red";
 ctx.beginPath();
 ctx.strokeStyle=color;
 ctx.lineWidth=2;
 ctx.arc(200,200,40,0,2*Math.PI);
-ctx.strokeStyle();
+ctx.stroke();
 canvas.addEventListener("mousedown",my_mousedown);
 
 function my_mousedown(e)
@@ -13,13 +13,13 @@ function my_mousedown(e)
     color=document.getElementById("color").value;
     console.log(color);
 
-    mouse_x=e.clientX-canvas.offsetLeft;
-    mouse_y=e.clienty-canvas.offsetTop;
+    mouse_x=e.clientX - canvas.offsetLeft;
+    mouse_y=e.clientY - canvas.offsetTop;
 
-    console.log("X="+mouse_x+",Y="+mouse_y);
+    console.log("X = " +mouse_x+ " ,Y = " +mouse_y);
    circle(mouse_x,mouse_y); 
 }
-function circle( mouse_x,mouse_y);
+function circle( mouse_x,mouse_y)
 {
     ctx.beginPath();
 ctx.strokeStyle=color;
@@ -30,5 +30,5 @@ ctx.stroke();
 
 function clearArea()
 {
-    ctx.clearRect(0,0,canves.width,canvas.height)
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
